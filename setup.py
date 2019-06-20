0c94ad2386f650af2cd9c68fecfe1778e7638ef3
Pedro Antonio Favuzzi <pa.favuzzi@gmail.com>
2019-06-20 19:16:29 +0200

    bump up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "tfExperiment",
-    version = "0.0.3",
+    version = "0.0.4",
     author = "Pedro A. Favuzzi",
     author_email = "pa.favuzzi@gmail.com",
     description = "A simple library to manage Tensorflow experiments though git and reduce boilerplate. Compatible with tf 1.x",
